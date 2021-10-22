8027ce49acbd6ef93c75e0897e4a1dec3b4a6b5e
Fred Balzi <balzi.fred@gmail.com>
2021-10-22 16:18:52 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <header>
           <h1>RSVP</h1>
-          <p>A Treehouse App</p>
+          <p>An Invitee Register</p>
           <form>
             <input type="text" value="Safia" placeholder="Invite Someone" />
             <button type="submit" name="submit" value="submit">Submit</button>
