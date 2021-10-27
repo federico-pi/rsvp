552dbf4f6ac40b16133c8086c2a5645c7100354f
Fred Balzi <balzi.fred@gmail.com>
2021-10-27 08:52:04 +0200

    attending, unconfirmed, total attendees

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import GuestList from './GuestList';
+import Counter from './Counter';
 
 class App extends Component {
 
@@ -90,8 +91,19 @@ class App extends Component {
   }
 
   getTotalInvited = () => this.state.guests.length;
+  
+  getAttendingGuests = () => 
+    this.state.guests.reduce(
+      (total, guest) => guest.isConfirmed ? total + 1 : total, 
+      0
+    )
 
   render() {
+    const totalInvited = this.getTotalInvited();
+    const numberAttending = this.getAttendingGuests();
+    const numberUnconfirmed = totalInvited - numberAttending;
+
+
     return (
       <div className="App">
         <header>
@@ -118,22 +130,11 @@ class App extends Component {
               /> Hide those who haven't responded
             </label>
           </div>
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
+          <Counter 
+            totalInvited={totalInvited}
+            numberAttending={numberAttending}
+            numberUnconfirmed={numberUnconfirmed}
+          />
           
           <GuestList 
             guests={this.state.guests}
@@ -142,8 +143,8 @@ class App extends Component {
             setNameAt={this.setNameAt}
             isFiltered={this.state.isFiltered}
             removeGuest={this.removeGuest}
+            pendingGuest={this.state.pendingGuest}
           />
-          
         </div>
       </div>
     );
--- src/GuestList.js
+++ src/GuestList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import Guest from "./Guest";
+import PendingGuest from "./PendingGuest";
 
 const GuestList = props => 
     <ul>
+        <PendingGuest name={props.pendingGuest} />
         { props.guests
             .filter(guest => !props.isFiltered || guest.isConfirmed)
             .map((guest, index) => 
@@ -25,7 +27,8 @@ GuestList.propTypes = {
     toggleEditing: PropTypes.func.isRequired,
     setNameAt: PropTypes.func.isRequired,
     isFiltered: PropTypes.bool.isRequired,
-    removeGuest: PropTypes.func.isRequired
+    removeGuest: PropTypes.func.isRequired,
+    pendingGuest: PropTypes.string.isRequired
 }
 
 export default GuestList;
